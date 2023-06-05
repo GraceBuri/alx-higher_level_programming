838831147badf65282ce3c7a3509fadf719f9982
GraceBuri <grcbuuri@gmail.com>
2023-06-05 14:03:12 -0700

    102-magic_calculation.py

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    result = 0
-    
-    if a > b:
-        result = (a * b) + 98
-    else:
-        result = (a - b) + 98
-        
+    result = a ** b
+    result += 98
     return result
