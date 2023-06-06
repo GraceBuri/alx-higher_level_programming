a74af95b6f07772b3fe79f1b77f78d9c22837b21
GraceBuri <grcbuuri@gmail.com>
2023-06-06 10:25:23 -0700

    debug to pycodestyle

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,4 +2,4 @@
 for char_code in range(97, 123):
     if char_code == 101 or char_code == 113:
         continue
-    print("{}".format(chr(char_code)) ,end="")
+    print("{}".format(chr(char_code)), end="")
