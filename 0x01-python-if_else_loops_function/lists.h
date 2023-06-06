9362a27f1d326e73c77a5bad8fa0fdf761eba6ff
GraceBuri <grcbuuri@gmail.com>
2023-06-06 08:10:07 -0700

    Python - if/else, loops, functions

--- 0x01-python-if_else_loops_function/lists.h
+++ 0x01-python-if_else_loops_function/lists.h
@@ -4,22 +4,23 @@
 #include <stdlib.h>
 
 /**
- * struct listint_s - single linked list
- * @a: int
- * @next: points next node
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
  *
- * Desc: single linked list node struct
+ * Description: singly linked list node structure
  *
  */
 typedef struct listint_s
 {
-	int a;
+	int n;
 	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
-listint_t *add_nodeint_end(listint_t **head, const int a);
+listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 listint_t *insert_node(listint_t **head, int number);
 
 #endif /* LISTS_H */
+
