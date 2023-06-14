703341fa6ea8ea729feb6673f3197c71b85c48a3
GraceBuri <grcbuuri@gmail.com>
2023-06-14 15:51:06 -0700

    Advanced

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    """Replace or add key/value pairs in a dictionary."""
+    if key in a_dictionary:
+        a_dictionary[key] = value
+    else:
+        a_dictionary.update({key: value})
+    
+    return a_dictionary
