2aa763b846ad2deca55f24a5c7af6b58bbd3fd04
GraceBuri <grcbuuri@gmail.com>
2023-06-27 17:41:54 +0300

    Classes and Objects Python

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
+"""Define class"""
+
+
 class Square:
+
+    """Class representation of a square
+    
+    Attribute:
+    None
+    Methods:
+    None
+
+
+    """
         pass
