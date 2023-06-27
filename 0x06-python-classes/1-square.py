3c0aa01b29439af24a35709de54c8256a17c5ec3
GraceBuri <grcbuuri@gmail.com>
2023-06-27 18:26:36 +0300

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python3
 
+class Square:
+    """Represent a square"""
+
+    def_init_(self, size):
+        """initialize new square
+
+
+        Args:
+        size(int): The size of the new square
+    """
+    self._size = size
+
+    def area(self):
+        """Calculate and return area of square"""
+        return self.self._size ** 2
