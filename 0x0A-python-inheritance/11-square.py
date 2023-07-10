463d137df4b22e09bd1027a2505c679243873dde
GraceBuri <grcbuuri@gmail.com>
2023-07-10 15:03:46 -0700

    fix task 11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
 """import rectangle subclass"""
-Rectangle = __import__('9-rectangle').Rectangle
+Base = __import__('7-base_geometry').BaseGeometry
 
 
-class Square(Rectangle):
+class Square(Base):
     """represents a square class"""
     def __init__(self, size):
         """initialize square sizes"""
         self.integer_validator("size", size)
-        super().__init__(size, size)
         self.__size = size
+
+    def __str__(self):
+        """print string rep of square"""
+        return '[Square]' + str(self.__size) + '/' + str(self.__size)
+
+    def area(self):
+        """calculate square area"""
+        return self.__size ** 2
