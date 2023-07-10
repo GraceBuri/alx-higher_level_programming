9f7c87aa1630b2d6e350b7d6cc4bbb418bb50cd1
GraceBuri <grcbuuri@gmail.com>
2023-07-10 13:01:58 -0700

    add task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Write a function that returns the list of available attributes and methods of an object"""
+
+
+
+def lookup(obj):
+    """return attributes and methods of objects"""
+    return dir(obj)
