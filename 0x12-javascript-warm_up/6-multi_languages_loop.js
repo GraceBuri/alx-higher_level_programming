9e09363ffa613eaf5c8f9bafe816ef31fefb9a69
GraceBuri <grcbuuri@gmail.com>
2023-09-11 18:50:00 -0700

    JS warmup

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const strings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let index = 0;
+
+while (index < strings.length) {
+  console.log(strings[index]);
+  index++;
+}
