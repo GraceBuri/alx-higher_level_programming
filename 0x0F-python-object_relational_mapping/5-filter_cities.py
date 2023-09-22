c04749322693c726db4c70a84047813ee76f51af
GraceBuri <grcbuuri@gmail.com>
2023-09-22 00:29:52 -0700

    finished Orm task

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     cursor = db.cursor()
     cursor.execute("SELECT cities.id, cities.name, states.name \
             FROM cities \
-            JOIN states ON cities.state_id = states.id; \
+            JOIN states ON cities.state_id = states.id\
             WHERE states.name = '{}';".format(sys.argv[4]))
     cities = cursor.fetchall()
 
