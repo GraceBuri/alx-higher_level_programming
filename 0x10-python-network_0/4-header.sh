595ccd9582415e65cac0882fe6ff64465b65d6e7
Grace Buri <grcbuuri@gmail.com>
2023-09-28 17:59:26 -0700

    add more python network tasks

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#writea script that sends a request and  returns body of resposnse
-curl -s -X GET -H "X-HolbertonSchool-User-Id: 98" "$1"
+#write a script that sends a request and  returns body of resposnse
+curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
